e53da726dc8309948bc5abfc4d3a1b3379933cce
poojithabada <poojithabada68@gmail.com>
2026-06-28 21:42:47 +0530

    Initial commit of MERN Task Tracker.

--- server/server.js
+++ server/server.js
@@ -1,41 +1,28 @@
 import express from "express";
 import dotenv from "dotenv";
 import cors from "cors";
-import path from 'path';
-import { fileURLToPath } from 'url';
 import connectDB from "./config/db.js";
 import taskRoutes from "./routes/taskRoutes.js";
 import authRoutes from "./routes/authRoutes.js";
 
-// Load env variables FIRST
 dotenv.config();
 
 const app = express();
-const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
-// Connect Database
 connectDB();
 
-// Middleware
 app.use(cors());
 app.use(express.json());
 
-// Routes
 app.use("/api/auth", authRoutes);
 app.use("/api/tasks", taskRoutes);
 
-// Production Build: Serve frontend
-if (process.env.NODE_ENV === 'production') {
-  // Try 'client/dist' if the server folder is at the root level
-  app.use(express.static(path.join(__dirname, '../client/dist')));
-  
-  app.get('/*', (req, res) => {
-  res.sendFile(path.join(__dirname, '../client/dist/index.html'));
+app.get("/", (req, res) => {
+  res.send("Task Tracker API Running...");
 });
-}
 
-// Start Server
 const PORT = process.env.PORT || 5000;
+
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
